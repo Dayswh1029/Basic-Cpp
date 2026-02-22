dc5f111c5f05fcebd9dabeb5937330d6ddffff74
Hengwang <149066349+Dayswh1029@users.noreply.github.com>
2026-02-22 10:16:53 +0100

    Update size_t.cpp

--- chapter2/size_t.cpp
+++ chapter2/size_t.cpp
@@ -2,12 +2,21 @@
 
 int main()
 {
-    // 介绍一个无符号的整数 size_t
+    // 介绍一个无符号的整数 size_t 
+    // 这是一个非负整数！ 用来 for 循环！
 
     size_t b=10;
     std::cout<<sizeof(size_t)<<std::endl;
+    // 不能n>=0 否则会死循环！ 因为size_t不可能小于0！
+    for(size_t n=2;n>0;n--)
+    {
+        std::cout<<"n= "<<n<<std::endl;
+    }
+
+    
 
 
 
     return 0;
+
 }
