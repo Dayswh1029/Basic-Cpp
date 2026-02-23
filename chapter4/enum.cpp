c5772bc3f40a05ff1549e1006ed9c641cc0af243
Hengwang <149066349+Dayswh1029@users.noreply.github.com>
2026-02-23 09:42:02 +0100

    Add files via upload
    需要注意 枚举的理解！ 以后需要补充！

---
+++ chapter4/enum.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include <cstdint> // 包含 int32_t 和 int8_t 的定义
+#include<cmath>
+enum color{ WHITE, BLACK, RED, GREEN,BLUE,YELLOW,NUM_COLORS};
+// NUM_COLORS  默认为6 ！ 可以随着改变！
+
+enum datatype{ TYPE_INT8=1, TYPE_INT16=2, TYPE_INT32=4, TYPE_INT64=8};
+struct Point{
+    enum datatype type;
+
+    union{
+        // 共享内存！
+        std::int8_t data8[3]; 
+        std::int16_t data16[3];
+        std::int32_t data32[3];
+        std::int64_t data64[3];
+
+    };
+};
+
+size_t datawidth(struct Point pt)
+{
+    return size_t(pt.type)*3;
+}
+
+int64_t l1norm(struct Point pt)  // 1-norm
+{
+    int64_t result=0;
+    switch(pt.type)
+    {
+        case (TYPE_INT8):
+            result =std::abs(pt.data8[0])+std::abs(pt.data8[1])+std::abs(pt.data8[2]);
+            break;
+        case (TYPE_INT16):
+            result =std::abs(pt.data16[0])+std::abs(pt.data16[1])+std::abs(pt.data16[2]);
+            break;
+        case (TYPE_INT32):
+            result =std::abs(pt.data32[0])+std::abs(pt.data32[1])+std::abs(pt.data32[2]);
+            break;
+        case (TYPE_INT64):
+            result =std::abs(pt.data64[0])+std::abs(pt.data64[1])+std::abs(pt.data64[2]);
+            break;
+        default:
+            break;
+
+    }
+    return result;
+
+}
+
+int main()
+{
+
+    enum color pen_color =RED;
+    pen_color=color(3);// convert int to enum
+    std::cout<<"we have "<<NUM_COLORS<<" pens."<<std::endl;
+
+
+    // pen_color +=1; // error
+    int  color_index=pen_color;
+    color_index +=1;
+    std::cout<<"color_index = "<<color_index<<std::endl;
+
+    // declaration and initialization 
+
+    Point point1 = {.type = TYPE_INT8, .data8={-2,3,4}};
+    Point point2 = {.type = TYPE_INT32, .data32={1,-2,3}};
+
+    std::cout<<"Data width = "<<datawidth(point1)<<std::endl;
+    std::cout<<"Data width = "<<datawidth(point2)<<std::endl;
+
+    std::cout<<"L1 norm = "<<l1norm(point1)<<std::endl;
+    std::cout<<"L1 norm = "<<l1norm(point2)<<std::endl;
+
+
+
+
+
+    return 0;
+}
