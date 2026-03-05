1914fdbd7edc8927638156a00786b522e22cd70e
Hengwang <149066349+Dayswh1029@users.noreply.github.com>
2026-03-05 12:52:38 +0100

    Update complex_class.cpp

--- Lab9/complex_class.cpp
+++ Lab9/complex_class.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
-// complex class
-
+// complex class exercise3 
 class Complex
 {
     private:
@@ -26,7 +25,6 @@ class Complex
             temp.real=this->real+c.real;
             temp.imag=this->imag+c.imag;
             return temp;  // 返回值 不可以是引用！ 
-        
         }
         //2. operator=
         Complex& operator=(const Complex& other)
@@ -48,7 +46,6 @@ class Complex
             return Complex(this->real*scalar,this->imag*scalar);
         }
         //5, Scalar *Complex
-      
         friend  Complex operator*(double scalar,const Complex& other)
         {
             return other*scalar;
@@ -59,14 +56,12 @@ class Complex
             this->real=(this->real+other.real);
             this->imag=(this->imag+other.imag);
             return *this;
-
         }
         //6, 一目运算符 operator-  不要碰 this ！返回一个 临时的complex
         Complex operator-()const
         {
             return Complex(-real,-imag);
         }
-
         //6, ostream& << 的overloading
         // 重载实现 一定注意！ 这个不是成员函数！是友元！ 
        friend std::ostream& operator<<(std::ostream& os, const Complex& other) {
@@ -75,7 +70,6 @@ class Complex
             return os; // 返回流本身，这是实现 cout << p1 << p2 << endl 的关键
         }
 };
-
 int main()
 {
     Complex c1;
@@ -86,6 +80,5 @@ int main()
     std::cout<<(c3*c2)<<std::endl;
     return 0;
 }
-
 
 //以后再慢慢的补充复数计算的重载！
